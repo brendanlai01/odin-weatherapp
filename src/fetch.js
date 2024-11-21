878c8e34b1a9555ee2684474d302814095a4041a
Brendan <103078608+brendanlai01@users.noreply.github.com>
2024-11-21 00:28:30 -0500

    create fetch.js

---
+++ src/fetch.js
@@ -0,0 +1,30 @@
+export function retrieveData(location){
+    return new Promise((resolve, reject) =>{
+      fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=us&key=PBCAQN2E5DGNC4CEYYD2KR2V8&contentType=json`, {
+        "method": "GET",
+        "headers": {
+        },
+        "mode": 'cors'
+        })
+      .then((response)=>{
+          return response.json();
+      })
+      .then((data) => {
+        resolve(data);
+        console.log(data);
+        console.log(data.description);
+      })
+      .catch((err) => {
+        reject(`Error: Data fetch failed - ${err.message}`);
+        console.error(err);
+      });
+    })
+}
+
+export async function processData(location){
+  try {
+    
+  } catch (error) {
+    
+  }
+}
