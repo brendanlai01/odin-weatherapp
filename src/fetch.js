4b2a2032b0db26a729c59567fa62021cf95194c0
Brendan <103078608+brendanlai01@users.noreply.github.com>
2025-01-12 03:38:54 -0500

    Update fetch.js

--- src/fetch.js
+++ src/fetch.js
@@ -1,18 +1,26 @@
-export function fetchData(location){
+import { loading, renderConditions, renderForecast, renderMain, restoreAppSkeleton } from "./render";
+
+export async function fetchData(location){
     return new Promise((resolve, reject) =>{
-      fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=us&key=PBCAQN2E5DGNC4CEYYD2KR2V8&contentType=json`, {
+      fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=us&include=current&key=PBCAQN2E5DGNC4CEYYD2KR2V8&contentType=json`, {
         "method": "GET",
         "headers": {
         },
         "mode": 'cors'
         })
       .then((response)=>{
+          loading();
           return response.json();
       })
       .then((data) => {
+        restoreAppSkeleton();
+        renderConditions(data);
+        renderMain(data);
+        renderForecast(data);
         resolve(data);
         console.log(data);
         console.log(data.description);
+        console.log(data.humidity);
       })
       .catch((err) => {
         reject(`Error: Data fetch failed - ${err.message}`);
